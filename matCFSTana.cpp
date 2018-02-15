38997093d79c8a9339bc906310cc68fe24a9113e
Yadong Jiang <yjiang@FEUPSIG.FE.UP.PT>
2018-02-15 18:22:21 +0000

    $(data)

--- matCFSTana.cpp
+++ matCFSTana.cpp
@@ -1,6 +1,50 @@
 #include "matCFSTana.h"
 
-matCFSTana::matCFSTana()
-{
+#include "matCFST.h"
+#include <iostream>
+#include <vector>
+#include <QPointF>
 
+matCFSTana::matCFSTana(double E, double f1, double f2, double b1, double b2, double revRatio) : matCFST(E, f1, f2, b1, b2, revRatio) {}
+
+matCFSTana::matCFSTana() {}
+
+matCFSTana::~matCFSTana() {}
+
+void matCFSTana::analysis(std::vector<double> loadingProtocol, double interval)
+{
+    this->reset();
+    this->loadingProtocol = loadingProtocol;
+    for (int i=0; i<this->loadingProtocol.size(); ++i) {
+        this->analysisOneStep(this->loadingProtocol[i], interval);
+    }
+}
+
+void matCFSTana::analysisOneStep(double target, double interval)
+{
+    if (this->strain <= target) {
+        while (this->strain < target) {
+            this->nextStress(this->strain + interval);
+            this->next();
+            this->Xresults.push_back(this->strain);
+            this->Yresults.push_back(this->stress);
+            this->points.push_back(QPointF(this->strain, this->stress));
+        }
+    } else {
+        while (this->strain > target) {
+            this->nextStress(this->strain - interval);
+            this->next();
+            this->Xresults.push_back(this->strain);
+            this->Yresults.push_back(this->stress);
+            this->points.push_back(QPointF(this->strain, this->stress));
+        }
+    }
+}
+
+void matCFSTana::reset()
+{
+    this->Xresults.clear();
+    this->Yresults.clear();
+    this->points.clear();
+    this->loadingProtocol.clear();
 }
--- matCFSTana.h
+++ matCFSTana.h
@@ -1,11 +1,29 @@
 #ifndef MATCFSTANA_H
 #define MATCFSTANA_H
 
+#include "matCFST.h"
+#include <iostream>
+#include <vector>
+#include <QPointF>
 
-class matCFSTana
+
+class matCFSTana : public matCFST
 {
 public:
+    matCFSTana(double E, double f1, double f2, double b1, double b2, double revRatio);
     matCFSTana();
+    ~matCFSTana();
+
+    void analysis(std::vector<double> loadingProtocol, double interval);
+
+    std::vector<double> Xresults, Yresults;
+    std::vector<double> loadingProtocol;
+    std::vector<QPointF> points;
+
+private:
+    void analysisOneStep(double target, double interval);
+    void reset();
+
 };
 
 #endif // MATCFSTANA_H
